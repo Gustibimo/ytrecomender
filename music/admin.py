d62573e5fc212709de16d7185442891229d44dad
Gustibimo <gstbimo@gmail.com>
2019-07-16 20:36:30 +0200

    Update models

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import NewVideo
+
+class NewVideoAdmin(admin.ModelAdmin):
+    list_display = [
+        'video_id'
+        'video_title',
+        'moods',
+        'labeled',
+        'predicted_moods',
+    ]
+    search_fields = [
+        'video_id',
+        'video_title',
+        'moods',
+    ]
+    list_editable = [
+        'moods',
+        'labeled',
+    ]
+
+admin.site.register(NewVideo, NewVideoAdmin)
